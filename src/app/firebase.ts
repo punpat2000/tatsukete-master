abcb03236e2ebb3436969879e5a313d57c5582f1
punpat2000 <saint12302@gmail.com>
2020-02-21 02:01:08 +0700

    update

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,12 +1,19 @@
 const config = {
-    apiKey: "AIzaSyDpash2FAkLL-0yre1axUjfyenlydPYXtI",
-    authDomain: "test-f82a6.firebaseapp.com",
-    databaseURL: "https://test-f82a6.firebaseio.com",
-    projectId: "test-f82a6",
-    storageBucket: "test-f82a6.appspot.com",
-    messagingSenderId: "977436487161",
-    appId: "1:977436487161:web:691b3d5c850f2898fca128",
-    measurementId: "G-QRX490VDZV"
-  }
+  apiKey: "AIzaSyC9-gjzb5ZRmIIFFJ7ja71smcfjipBhwPY",
+  authDomain: "chat-app-by-nae.firebaseapp.com",
+  databaseURL: "https://chat-app-by-nae.firebaseio.com",
+  projectId: "chat-app-by-nae",
+  storageBucket: "chat-app-by-nae.appspot.com",
+  messagingSenderId: "314485715156",
+  appId: "1:314485715156:web:ec04f77848b8191407cb41"
+  // apiKey: "AIzaSyDpash2FAkLL-0yre1axUjfyenlydPYXtI",
+  // authDomain: "test-f82a6.firebaseapp.com",
+  // databaseURL: "https://test-f82a6.firebaseio.com",
+  // projectId: "test-f82a6",
+  // storageBucket: "test-f82a6.appspot.com",
+  // messagingSenderId: "977436487161",
+  // appId: "1:977436487161:web:691b3d5c850f2898fca128",
+  // measurementId: "G-QRX490VDZV"
+}
 
-  export default config 
+export default config 
